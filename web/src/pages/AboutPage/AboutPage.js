fb5ef9a8f31a24bc16c1c802aa984035553509bd
Averyrj <averyj54321@gmail.com>
2022-10-04 15:32:35 +1100

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+//import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>About this page</p>
+    </>
+  )
+}
+
+export default AboutPage
